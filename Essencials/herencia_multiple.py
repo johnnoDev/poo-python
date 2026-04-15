915814e110f3dd19658b6dc61b62ebf4970ed634
johnnoDev <johnny.lopezvera20@gmail.com>
2026-04-15 18:34:41 -0500

    hola

---
+++ Essencials/herencia_multiple.py
@@ -0,0 +1,15 @@
+class Volador:
+    def mover(self):
+        print('Estoy volando por el aire')
+
+class Nadador:
+    def mover(self):
+        print('Estoy nadando en el agua')
+
+# Cramos una clase 'Pato' que hereda de ambas
+class Pato(Volador, Nadador):
+    pass
+
+mi_pato = Pato()
+mi_pato.mover()
+print(Pato.mro())
