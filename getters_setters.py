6500ee2a078e34a1cf9966a01db54ad6925a0d9d
johnnoDev <johnny.lopezvera20@gmail.com>
2026-04-16 16:04:20 -0500

    getter y setters

--- getters_setters.py
+++ getters_setters.py
@@ -6,8 +6,14 @@ class Persona:
     def get_nombre(self):
         return self.__nombre
 
-        
+    # Permite modificar/escribir atributos muy protegidos
+    def set_nombre(self, nuevo_nombre):
+        self.__nombre = nuevo_nombre
+        return nuevo_nombre
 
 
 johnny = Persona('Johnny')
 print(johnny.get_nombre())
+
+caiza = Persona('Caiza')
+print(caiza.set_nombre('Zaulo'))
